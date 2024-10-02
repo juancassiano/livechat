383b222b167f96a2f5a039ce4875cbdf393758f9
Juan Lira <juancassiano@hotmail.com>
2024-10-01 21:32:20 -0300

    config: Configuração do websocket

---
+++ src/main/java/com/livechat/livechat/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.livechat.livechat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("buildrun-livechat-websocket");
+  }
+  
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topics");
+    registry.setApplicationDestinationPrefixes("/app");
+  }
+  
+}
